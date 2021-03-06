0c4bbc6d94e6a0ba14d4f3eb6a82e2cdef51f841
Mehrdad Tajedini <mehrta@LIVE.COM>
2021-03-06 20:08:45 +0330

    work on locales

--- src/locale.ts
+++ src/locale.ts
@@ -1,8 +1,18 @@
 export interface Locale {
+    /** Locale name */
     name: string;
+
+    week: {
+        /** The first day of the week */
+        first: number;
+
+        /** An array of the weekday names */
+        days: string[];
+    }
+
     calendars: {
         [calendar: string]: {
-            weekDays: string[];
+            /** An array of the month names */
             months: string[];
         }
     };
