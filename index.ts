07135a4344a732d883211a52889ca49c59758bd2
Mehrdad Tajedini <mehrta@LIVE.COM>
2021-03-18 18:43:34 +0330

    work on dev env

---
+++ index.ts
@@ -0,0 +1,5 @@
+console.log('asd');
+
+import { PersianDate } from './src/dates/persian';
+const pd = new PersianDate();
+console.log(pd);
--- src/date-time.spec.ts
+++ src/date-time.spec.ts
@@ -1,8 +1,12 @@
 // const assert = require("assert");
 // const Jalaali = require("date/index");
- 
+
 // describe("Demo", () => {
 //   it("should add correctly", () => assert.equal(2, 2));
 // });
+import { PersianDate } from './dates/persian';
 
+const p = new PersianDate()
 console.log('asd');
+console.log('asd');
+
