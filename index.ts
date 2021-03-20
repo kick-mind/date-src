2cae2de109ca7e5225f61bfbbe4985bdec68bf8e
Mehrdad Tajedini <mehrta@LIVE.COM>
2021-03-20 16:57:22 +0330

    small bugfix

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { PersianDate } from './src/dates/persian';
+import { PersianDate } from './src/date';
 
-const pd = new PersianDate({ year: 1400, month: 1, day: 1, hour: 0, minute: 0, second: 0, ms: 0 });
+const pd = new PersianDate(1400);
 const f = pd.format('Y4:M:D');
 
 console.log(f);
