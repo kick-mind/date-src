19fc51b0a33be5f92a573f74d1e6485ed4fc4aae
Mehrdad Tajedini <mehrta@LIVE.COM>
2021-03-22 14:50:22 +0430

    work on api

--- src/locale/system-locale.ts
+++ src/locale/system-locale.ts
@@ -1,7 +1,11 @@
 import { Locale, LocaleData } from './locale';
 
 export class SystemLocale extends Locale {
-    constructor(data: LocaleData) {
+    private constructor(data: LocaleData) {
         super(data);
+    }
+
+    static get(id: string): SystemLocale {
+        throw new Error('Method not implemented.');
     }
 }
