8802fb59f1599958fcab7bd42ee3ef207a8422bd
devui1085 <devui1085@gmail.com>
2021-03-31 17:34:31 +0000

    refactor gergorian2 calendar: move private methods

--- src/calendar/gregorian/gregorian2.ts
+++ src/calendar/gregorian/gregorian2.ts
@@ -50,6 +50,72 @@ const _daysToMonth366 = [
   335,
   366,
 ];
+function isLeapYear(year: number): boolean {
+  return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+}
+function getDateUnits(ticks: number): DateTimeUnits {
+  const du: DateTimeUnits = {
+    year: 0,
+    month: 0,
+    day: 0,
+    hour: 0,
+    minute: 0,
+    second: 0,
+    ms: 0,
+  };
+
+  let n = Math.trunc(ticks / msPerDay);
+  const y400 = Math.trunc(n / _daysPer400Years);
+  n -= y400 * _daysPer400Years;
+  let y100 = Math.trunc(n / _daysPer100Years);
+  if (y100 == 4) {
+    y100 = 3;
+  }
+  n -= y100 * _daysPer100Years;
+  const y4 = Math.trunc(n / _daysPer4Years);
+  n -= y4 * _daysPer4Years;
+  let y1 = Math.trunc(n / _daysPerYear);
+  if (y1 == 4) {
+    y1 = 3;
+  }
+  du.year = y400 * 400 + y100 * 100 + y4 * 4 + y1 + 1;
+  n -= y1 * _daysPerYear;
+  const leapYear: boolean = y1 == 3 && (y4 != 24 || y100 == 3);
+  const days = leapYear ? _daysToMonth366 : _daysToMonth365;
+  // tslint:disable-next-line: no-bitwise
+  let m = (n >> 5) + 1;
+  // m = 1-based month number
+  while (n >= days[m]) {
+    m++;
+  }
+  du.month = m;
+  du.day = n - days[m - 1] + 1;
+
+  return du;
+}
+
+function getAbsoluteDate(year: number, month: number, day: number): number {
+  if (year >= 1 && year <= _maxYear && month >= 1 && month <= 12) {
+    const days = isLeapYear(year) ? _daysToMonth366 : _daysToMonth365;
+    if (day >= 1 && day <= days[month] - days[month - 1]) {
+      const y = year - 1;
+      const absoluteDate =
+        y * 365 +
+        Math.trunc(y / 4) -
+        Math.trunc(y / 100) +
+        Math.trunc(y / 400) +
+        days[month - 1] +
+        day -
+        1;
+      return absoluteDate;
+    }
+  }
+  throwInvalidParam();
+}
+
+function dateToTicks(year: number, month: number, day: number): number {
+  return getAbsoluteDate(year, month, day) * msPerDay;
+}
 
 /** Gregorian2 calendar */
 export class GregorianCalendar2 extends Calendar {
@@ -62,8 +128,6 @@ export class GregorianCalendar2 extends Calendar {
     let y = ut.year;
     let m = ut.month;
     let d = ut.day;
-    // int y, m, d;
-    // this.GetDatePart(time.Ticks, out y, out m, out d);
 
     const i = m - 1 + months;
     if (i >= 0) {
@@ -80,7 +144,7 @@ export class GregorianCalendar2 extends Calendar {
       d = days;
     }
     const ticks =
-      this.dateToTicks(y, m, d) + (getCalendarTimestamp(time) % msPerDay);
+      dateToTicks(y, m, d) + (getCalendarTimestamp(time) % msPerDay);
     return getJsTimestamp(ticks);
   }
   addYears(time: number, years: number): number {
@@ -101,7 +165,7 @@ export class GregorianCalendar2 extends Calendar {
     return this.isLeapYear(year) ? 366 : 365;
   }
   isLeapYear(year: number): boolean {
-    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    return isLeapYear(year);
   }
   getTimestamp(units: DateTimeUnits): number {
     const u = units;
@@ -118,70 +182,7 @@ export class GregorianCalendar2 extends Calendar {
 
   getUnits(ts: number): DateTimeUnits {
     ts = getCalendarTimestamp(ts);
-    return { ...this.getDateUnits(ts), ...getTimeUnits(ts) };
-  }
-
-  private getDateUnits(ticks: number): DateTimeUnits {
-    const du: DateTimeUnits = {
-      year: 0,
-      month: 0,
-      day: 0,
-      hour: 0,
-      minute: 0,
-      second: 0,
-      ms: 0,
-    };
-
-    let n = Math.trunc(ticks / msPerDay);
-    const y400 = Math.trunc(n / _daysPer400Years);
-    n -= y400 * _daysPer400Years;
-    let y100 = Math.trunc(n / _daysPer100Years);
-    if (y100 == 4) {
-      y100 = 3;
-    }
-    n -= y100 * _daysPer100Years;
-    const y4 = Math.trunc(n / _daysPer4Years);
-    n -= y4 * _daysPer4Years;
-    let y1 = Math.trunc(n / _daysPerYear);
-    if (y1 == 4) {
-      y1 = 3;
-    }
-    du.year = y400 * 400 + y100 * 100 + y4 * 4 + y1 + 1;
-    n -= y1 * _daysPerYear;
-    const leapYear: boolean = y1 == 3 && (y4 != 24 || y100 == 3);
-    const days = leapYear ? _daysToMonth366 : _daysToMonth365;
-    // tslint:disable-next-line: no-bitwise
-    let m = (n >> 5) + 1;
-    // m = 1-based month number
-    while (n >= days[m]) {
-      m++;
-    }
-    du.month = m;
-    du.day = n - days[m - 1] + 1;
-
-    return du;
-  }
-
-  private getAbsoluteDate(year: number, month: number, day: number): number {
-    if (year >= 1 && year <= _maxYear && month >= 1 && month <= 12) {
-      const days = this.isLeapYear(year) ? _daysToMonth366 : _daysToMonth365;
-      if (day >= 1 && day <= days[month] - days[month - 1]) {
-        const y = year - 1;
-        const absoluteDate =
-          y * 365 +
-          Math.trunc(y / 4) -
-          Math.trunc(y / 100) +
-          Math.trunc(y / 400) +
-          days[month - 1] +
-          day -
-          1;
-        return absoluteDate;
-      }
-    }
-    throwInvalidParam();
-  }
-  private dateToTicks(year: number, month: number, day: number): number {
-    return this.getAbsoluteDate(year, month, day) * msPerDay;
+    return { ...getDateUnits(ts), ...getTimeUnits(ts) };
   }
 }
 
