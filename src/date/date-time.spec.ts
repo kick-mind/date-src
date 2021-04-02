db856e302e7965b1d01b0ce287903bcb1d9229c8
Mehrdad Tajedini <mehrta@LIVE.COM>
2021-04-02 22:31:59 +0430

    switch to MOCHA test framework

--- src/date/date-time.spec.ts
+++ src/date/date-time.spec.ts
@@ -1,3 +1,4 @@
+import assert from 'assert';
 import { Locales } from '../locale';
 import { Zones } from '../zone';
 import { DateTime } from './date-time';
@@ -6,25 +7,25 @@ describe('DateTime', () => {
   it('create without parameter', () => {
     const d = new DateTime();
     const jd = new Date();
-    expect(d.year).toBe(jd.getFullYear());
-    expect(d.month).toBe(jd.getMonth());
-    expect(d.day).toBe(jd.getDate());
-    expect(d.minute).toBe(jd.getMinutes());
-    expect(d.second).toBe(jd.getSeconds());
+    assert.strictEqual(d.year, jd.getFullYear());
+    assert.strictEqual(d.month, jd.getMonth());
+    assert.strictEqual(d.day, jd.getDate());
+    assert.strictEqual(d.minute, jd.getMinutes());
+    assert.strictEqual(d.second, jd.getSeconds());
   });
 
   it('create from timestamp', () => {
     const d1 = new DateTime(12345);
-    expect(d1.year).toBe(1000);
-    expect(d1.month).toBe(1);
-    expect(d1.day).toBe(1);
+    assert.strictEqual(d1.year, 1000);
+    assert.strictEqual(d1.month, 1);
+    assert.strictEqual(d1.day, 1);
     // Do more validation on d1 here...
 
 
     const d2 = new DateTime(12345, { calendar: 'persian' });
-    expect(d2.year).toBe(1000);
-    expect(d2.month).toBe(1);
-    expect(d2.day).toBe(1);
+    assert.strictEqual(d2.year, 1000);
+    assert.strictEqual(d2.month, 1);
+    assert.strictEqual(d2.day, 1);
     // Do more validation on d2 here...
 
   });
@@ -33,17 +34,17 @@ describe('DateTime', () => {
     const l = Locales.find('fa-IR');
     const z = Zones.utc;
     const d = new DateTime(2001, 9, 8, 18, 5, 4, 90, { locale: l, zone: z });
-    expect(d.format('Y YY YYYY')).toBe('2001 01 2001');
-    expect(d.format('M MM MMM MMMM')).toBe('9 09 نوامبر نوامبر');
-    expect(d.format('d dd')).toBe('8 08');
-    expect(d.format('H HH h hh')).toBe('18 18 6 06');
-    expect(d.format('m mm')).toBe('5 05');
-    expect(d.format('s ss')).toBe('4 04');
-    expect(d.format('f fff')).toBe('90 090');
-    expect(d.format('c cc C CC CCC')).toBe('');
-    expect(d.format('z zz zzz Z ZZ ZZZ')).toBe('0 00:00 0000 UTC utc utc');
-    expect(d.format('[Now:] YYYY/MM/dd HH:mm:ss')).toBe('Now: 2001/09/08 18:05:04');
-    expect(d.format('[This Text and [this text] should be skipped] [and the date is:] YYYY/MM/dd'))
-      .toBe('This Text and [this text] should be skipped and the date is: 2001/09/08');
+    assert.strictEqual(d.format('Y YY YYYY'), '2001 01 2001');
+    assert.strictEqual(d.format('M MM MMM MMMM'), '9 09 نوامبر نوامبر');
+    assert.strictEqual(d.format('d dd'), '8 08');
+    assert.strictEqual(d.format('H HH h hh'), '18 18 6 06');
+    assert.strictEqual(d.format('m mm'), '5 05');
+    assert.strictEqual(d.format('s ss'), '4 04');
+    assert.strictEqual(d.format('f fff'), '90 090');
+    assert.strictEqual(d.format('c cc C CC CCC'), '');
+    assert.strictEqual(d.format('z zz zzz Z ZZ ZZZ'), '0 00:00 0000 UTC utc utc');
+    assert.strictEqual(d.format('[Now:] YYYY/MM/dd HH:mm:ss'), 'Now: 2001/09/08 18:05:04');
+    assert.strictEqual(d.format('[This Text and [this text] should be skipped] [and the date is:] YYYY/MM/dd'),
+      'This Text and [this text] should be skipped and the date is: 2001/09/08');
   });
 });
---
+++ src/locale/js-locale.spec.ts
@@ -0,0 +1,106 @@
+import assert from 'assert';
+import { JsLocale, Locales } from '.';
+import { Calendars } from '../calendar';
+import { PersianCalendar } from '../calendar/persian/persian';
+
+Calendars.add(new PersianCalendar());
+
+describe('JsLocale', () => {
+  it('can get long month names of the gregorian calendar', () => {
+    const l1 = new JsLocale('fa', { weekStart: 6 });
+    assert.strictEqual(l1.getMonthNames(Calendars.findById('gregorian'), 'long'), [
+      'ژانویه',
+      'فوریه',
+      'مارس',
+      'آوریل',
+      'مه',
+      'ژوئن',
+      'ژوئیه',
+      'اوت',
+      'سپتامبر',
+      'اکتبر',
+      'نوامبر',
+      'دسامبر',
+    ]);
+  });
+
+  it('can get long month names of the persian calendar', () => {
+    const l1 = new JsLocale('en', { weekStart: 6 });
+    assert.strictEqual(l1.getMonthNames(Calendars.findById('persian'), 'long'), [
+      'Farvardin',
+      'Ordibehesht',
+      'Khordad',
+      'Tir',
+      'Mordad',
+      'Shahrivar',
+      'Mehr',
+      'Aban',
+      'Azar',
+      'Dey',
+      'Bahman',
+      'Esfand',
+    ]);
+  });
+
+  it('can get short month names', () => {
+    const l1 = new JsLocale('en', { weekStart: 6 });
+    assert.strictEqual(l1.getMonthNames(Calendars.findById('gregorian'), 'short'), [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ]);
+  });
+
+  it('can get narrow month names', () => {
+    const l1 = new JsLocale('fa', { weekStart: 6 });
+    assert.strictEqual(l1.getMonthNames(Calendars.findById('gregorian'), 'narrow'), [
+      'ژ',
+      'ف',
+      'م',
+      'آ',
+      'م',
+      'ژ',
+      'ژ',
+      'ا',
+      'س',
+      'ا',
+      'ن',
+      'د',
+    ]);
+  });
+
+  it('can get long weekday names', () => {
+    const l1 = new JsLocale('fa', { weekStart: 6 });
+    assert.strictEqual(l1.getWeekdayNames(), [
+      'شنبه',
+      'یکشنبه',
+      'دوشنبه',
+      'سه‌شنبه',
+      'چهارشنبه',
+      'پنجشنبه',
+      'جمعه',
+    ]);
+  });
+
+  it('can get narrow weekday names', () => {
+    const l1 = new JsLocale('fa', { weekStart: 6 });
+    assert.strictEqual(l1.getWeekdayNames('narrow'), [
+      'ش',
+      'ی',
+      'د',
+      'س',
+      'چ',
+      'پ',
+      'ج',
+    ]);
+  });
+});
