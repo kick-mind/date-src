cd54ecbd2ce1ff85365b9b980acbf1d816a98dff
mehdi bayat <mehdi.byt@live.com>
2021-04-25 01:08:44 +0430

    days-in-month

---
+++ src/plugins/days-in-month.spec.ts
@@ -0,0 +1,24 @@
+import assert from "assert";
+import { DateTime } from "../date-time";
+import { daysInMonth } from "./days-in-month";
+import moment from "moment";
+
+describe("Plugins", () => {
+  describe("days-in-month", () => {
+    it("can compare with moment for all 12 months in a year", () => {
+      const dt = new DateTime();
+
+      const months = dt.locale.getMonthNames(dt.calendar).map((e, i) => {
+        let today = new Date();
+        let newTimeInMonth = new DateTime(today.getFullYear(), i + 1, 1);
+        let r = daysInMonth(newTimeInMonth);
+        assert.strictEqual(
+          moment(
+            `${newTimeInMonth.year}-${newTimeInMonth.month}`
+          ).daysInMonth(),
+          daysInMonth(newTimeInMonth)
+        );
+      });
+    });
+  });
+});
--- src/plugins/parse.ts
+++ src/plugins/parse.ts
@@ -1,7 +1,10 @@
-import { CalendarSpecifier } from '../common';
-import { DateTime } from '../date-time';
+import { CalendarSpecifier } from "../common";
+import { DateTime } from "../date-time";
 
-export function parse(date: string, format: string, opts?: { calendar?: CalendarSpecifier }): DateTime {
-    
-    return;
+export function parse(
+  date: string,
+  format: string,
+  opts?: { calendar?: CalendarSpecifier }
+): DateTime {
+  return;
 }
