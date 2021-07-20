a36b5b1b3f391252d57f036b49571fe5ae87c29f
Mehrdad Tajedini <mehrta@LIVE.COM>
2021-07-20 23:24:07 +0430

    webpack configs

--- test.spec.js
+++ test.spec.js
@@ -1,6 +1,8 @@
 
 const jss = require('./dist');
 const { PersianCalendar } = require('./dist/calendars/persian');
+const { dayOfYear } = require('./dist/plugins/day-of-year');
+
 var x = new PersianCalendar();
 
 const d = new jss.DateTime();
--- webpack.config.js
+++ webpack.config.js
@@ -3,70 +3,61 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const fs = require("fs");
 const path = require("path");
 
-function getPluginsEntries() {
-    const arr = [];
-    const files = fs.readdirSync("./src/plugins");
-    const plugins = files.filter(x => !x.toLocaleLowerCase().endsWith('spec.ts'));
+function getCalendarsEntries() {
+    const files = fs.readdirSync("./src/calendars");
+    const calendars = files.filter(x => !x.toLocaleLowerCase().endsWith('spec.ts'));
     const entries = {};
 
-    // files.forEach((file) => {
-    //     entries[file.substr(0, file.length - 3)] = {
-    //         import: "./src/locale/repo/".concat(file),
-    //         dependOn: "main",
-    //     };
-    // });
+    calendars.forEach((cal) => {
+        const name = path.parse(cal).name;
+        entries[name] = {
+            import: `./src/calendars/${cal}`,
+            filename: `./calendars/${name}.js`,
+            dependOn: "main",
+            library: {
+                type: 'umd',
+            }
+        };
+    });
 
     return entries;
 }
 
-const entries = {
-    main: {
-        import: "./src/main/index.ts",
-        filename: 'main/jss-date.js',
-        library: {
-            name: 'jss',
-            type: 'umd',
-        },
-    },
-    gregorian: {
-        import: "./src/calendars/gregorian.ts",
-        filename: 'calendars/gregorian.js',
-        dependOn: "main",
-        library: {
-            type: 'umd',
-        }
-    },
-    gregorian2: {
-        import: "./src/calendars/gregorian2.ts",
-        filename: 'calendars/gregorian2.js',
-        dependOn: "main",
-        library: {
-            type: 'umd',
-        }
-    },
-    persian: {
-        import: "./src/calendars/persian.ts",
-        filename: 'calendars/persian.js',
-        dependOn: "main",
-        library: {
-            type: 'umd',
-        }
-    },
-    hijri: {
-        import: "./src/calendars/hijri.ts",
-        filename: 'calendars/hijri.js',
-        dependOn: "main",
-        library: {
-            type: 'umd',
-        }
-    },
-    ...getPluginsEntries()
-};
+function getPluginsEntries() {
+    const files = fs.readdirSync("./src/plugins");
+    const plugins = files.filter(x => !x.toLocaleLowerCase().endsWith('spec.ts'));
+    const entries = {};
+
+    plugins.forEach((plugin) => {
+        const name = path.parse(plugin).name;
+        entries[name] = {
+            import: `./src/plugins/${plugin}`,
+            filename: `./plugins/${name}.js`,
+            dependOn: "main",
+            library: {
+                type: 'umd',
+            }
+        };
+    });
+
+    return entries;
+}
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
     mode: 'production',
-    entry: entries,
+    entry: {
+        main: {
+            import: "./src/main/index.ts",
+            filename: 'main/jss-date.js',
+            library: {
+                name: 'jss',
+                type: 'umd',
+            },
+        },
+        ...getCalendarsEntries(),
+        ...getPluginsEntries()
+    },
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].js",
