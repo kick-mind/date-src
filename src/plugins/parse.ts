7ccd33376df904c0179a0f505a499c2a04f61069
Mehrdad Tajedini <mehrta@LIVE.COM>
2021-07-20 19:05:18 +0430

    update packages

--- src/plugins/parse.ts
+++ src/plugins/parse.ts
@@ -1,9 +1,9 @@
-import { DateTime, CalendarSpecifier } from '../main';
+import { DateTime, CalendarSpecifier, ZoneSpecifier, LocaleSpecifier } from '../main';
 
 export function parse(
   date: string,
   format: string,
-  opts?: { calendar?: CalendarSpecifier }
+  opts?: { calendar?: CalendarSpecifier, zone?: ZoneSpecifier, locale?: LocaleSpecifier }
 ): DateTime {
   return;
 }
