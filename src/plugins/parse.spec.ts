b291e17801580f1112b44854f7df2215c5a6841a
Mehrdad Tajedini <mehrta@LIVE.COM>
2021-07-21 18:43:07 +0430

    spec bugfix

--- src/plugins/parse.spec.ts
+++ src/plugins/parse.spec.ts
@@ -1,12 +1,22 @@
 import assert from 'assert';
-import { DateTime } from '../main';
+import { Calendars, DateTime } from '../main';
+import { GregorianCalendar } from '../calendars/gregorian';
+import { PersianCalendar } from '../calendars/persian';
 import { parse } from './parse';
 
 describe('Plugins', () => {
   describe('Parse', () => {
+    before(function () {
+      Calendars.add(new GregorianCalendar('gregorian'));
+      Calendars.add(new PersianCalendar('persian'));
+    });
+
     it('can parse', () => {
-      const d = parse('99/2/6', 'YY/M/D');
-      assert.ok(d.year == 1399 && d.month == 2 && d.day == 6);
+      const d = parse('2012/2/6', 'YY/M/D');
+      assert.deepStrictEqual(
+        [d.year, d.month, d.day,],
+        [2012, 2, 6]
+      );
     });
   });
 });
