822ce0ae2754abb2e81193a2df00bce30654a248
Mehrdad Tajdini <34556599+mehrta@users.noreply.github.com>
2021-12-05 05:18:45 -0800

    update generator

--- utils/locale-generator/generator.js
+++ utils/locale-generator/generator.js
@@ -1,7 +1,22 @@
 const fs = require('fs');
 const path = require('path');
+const { RuntimeLocale } = require('../../dist');
 
-const lines = fs.readFileSync(path.resolve(__dirname, './locales.txt'), 'utf8').split(/\r?\n/);
-lines.forEach(l => {
-    console.log(l);
+const locales = fs.readFileSync(path.resolve(__dirname, './locales.txt'), 'utf8').split(/\r?\n/);
+
+locales.forEach(localeName => {
+    const filePath = `./output/${localeName}.js`;
+
+    try {
+        const rl = new RuntimeLocale(localeName);
+        staticLocaleData = {
+
+        };
+
+        fs.writeFile(filePath, 'ok', () => {});
+    } catch (e) {
+        console.log('Locale generation failed for locale: ' + localeName);
+    }
+
+    // console.log(rl.getWeekdayNames('short'));
 });
