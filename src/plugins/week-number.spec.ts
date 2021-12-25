c6af3b9366b89507309f152b0056eaa2366077dd
devui1085 <devui1085@gmail.com>
2021-12-25 07:44:05 +0330

    update weeknumber unit test

--- src/plugins/week-number.spec.ts
+++ src/plugins/week-number.spec.ts
@@ -1,11 +1,17 @@
 import { weekNumber } from './week-number';
-import { DateTime } from '../main';
+import { Calendars, DateTime } from '../main';
+import { GregorianCalendar } from '../calendars/gregorian';
 import assert from 'assert';
 import moment from 'moment';
 
 describe('Plugins', () => {
   describe('weekNumber', () => {
     it('weekNumber', () => {
+      Calendars.add(new GregorianCalendar('gregorian'));
+      const dt1 = new DateTime(2005, 1, 1);
+
+      console.log(weekNumber(dt1));
+
       const dt = new DateTime(2021, 1, 1, 0, 0, 0, 0, {
         zone: 'Asia/Tehran',
         locale: 'fa',
