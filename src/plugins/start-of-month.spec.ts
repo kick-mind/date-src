bf98140ab89d60071a78e5c4494ae398b28e024a
devui1085 <devui1085@gmail.com>
2022-01-01 08:58:30 +0330

    add startOfMonth unittest

--- src/plugins/start-of-month.spec.ts
+++ src/plugins/start-of-month.spec.ts
@@ -3,6 +3,7 @@ import { DateTime, Calendars } from '../main';
 import { PersianCalendar } from '../calendars/persian';
 import { startOfMonth } from '../plugins/start-of-month';
 import { isSame } from '../plugins/is-same';
+import { startOfMonth as fnsStartOfMonth } from 'date-fns';
 describe('Plugins', () => {
   describe('start-of-month', () => {
     before(function () {
@@ -17,5 +18,24 @@ describe('Plugins', () => {
         isSame(dt2, new DateTime(2021, 11, 1, 0, 0, 0, 0))
       );
     });
+
+
+    it('fns vs jss', () => {
+      for (let i = 0; i < 1000; i++) {
+        let year = Math.floor(Math.random() * 22) + 2001;
+        let month = Math.floor(Math.random() * 12);
+        let day = Math.floor(Math.random() * 28) + 1;
+
+        const dt = new DateTime(year, month + 1, day);
+        const sdt = startOfMonth(dt);
+        console.log(sdt.year, sdt.month, sdt.day);
+        const dt2 = new Date(year, month, day);
+        const sdt2 = fnsStartOfMonth(dt2);
+        console.log(sdt2.getFullYear(), sdt2.getMonth() + 1, sdt2.getDate());
+
+        assert.deepStrictEqual({ year: sdt.year, month: sdt.month, day: sdt.day }, { year: sdt2.getFullYear(), month: sdt2.getMonth() + 1, day:sdt2.getDate() });
+      }
+    });
+
   });
 });
