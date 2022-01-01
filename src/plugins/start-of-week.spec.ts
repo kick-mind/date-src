f5499f2a3f3521345589c59216494d25200ed35e
devui1085 <devui1085@gmail.com>
2022-01-01 08:54:21 +0330

    add start of week unittest

--- src/plugins/start-of-week.spec.ts
+++ src/plugins/start-of-week.spec.ts
@@ -4,11 +4,13 @@ import { PersianCalendar } from '../calendars/persian';
 import { GregorianCalendar } from '../calendars/gregorian';
 import { startOfWeek } from '../plugins/start-of-week';
 import { isSame } from '../plugins/is-same';
+import { startOfWeek as fnsStartOfWeek } from 'date-fns';
+
 describe('Plugins', () => {
   describe('start-of-week', () => {
     before(function () {
-      Calendars.add(new PersianCalendar('persian'));
       Calendars.add(new GregorianCalendar('gregorian'));
+      Calendars.add(new PersianCalendar('persian'));
     });
     it('get start of week', () => {
       const dt = new DateTime(1400, 9, 18, 5, 45, 56, 3);
@@ -30,6 +32,23 @@ describe('Plugins', () => {
         true,
         isSame(dt2, new DateTime(2021, 9, 26, 0, 0, 0, 0))
       );
+    });
+
+    it('fns vs jss', () => {
+      for (let i = 0; i < 1000; i++) {
+        let year = Math.floor(Math.random() * 22) + 2001;
+        let month = Math.floor(Math.random() * 12);
+        let day = Math.floor(Math.random() * 28) + 1;
+
+        const dt = new DateTime(year, month + 1, day);
+        const sdt = startOfWeek(dt, 0);
+        console.log(sdt.year, sdt.month, sdt.day);
+        const dt2 = new Date(year, month, day);
+        const sdt2 = fnsStartOfWeek(dt2, { weekStartsOn: 0 });
+        console.log(sdt2.getFullYear(), sdt2.getMonth() + 1, sdt2.getDate());
+
+        assert.deepStrictEqual({ year: sdt.year, month: sdt.month, day: sdt.day }, { year: sdt2.getFullYear(), month: sdt2.getMonth() + 1, day:sdt2.getDate() });
+      }
     });
   });
 });
